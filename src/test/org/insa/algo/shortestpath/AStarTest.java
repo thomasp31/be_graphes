61dc0e615ef8707b72216fce1bf0b2c63768df57
Morvan Nozahic <morvan.nozahic@student.tut.fi>
2019-05-30 14:23:49 +0200

    insertion des résultat dans un fichier texte

--- src/test/org/insa/algo/shortestpath/AStarTest.java
+++ src/test/org/insa/algo/shortestpath/AStarTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import org.insa.graph.io.GraphReader;
 import org.insa.graph.io.PathReader;
 
 import org.junit.Test;
+import java.io.File;
 
 public class AStarTest {
 
@@ -29,13 +31,25 @@ public class AStarTest {
 		//String mapName = "C:/Users/Katillï¿½r/Documents/INSA/3ir/be_graphes/insa/europe/france/insa.mapgr";
 		//String pathName = "C:/Users/Katillï¿½r/Documents/INSA/3ir/be_graphes/insa/europe/france/path_fr31insa_rangueil_r2.path";
 		
+		//***************Path Ordinateur Morvan map BZH*********************/
+		//String mapName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/bretagne.mapgr";
+		//String pathName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/bretagne.path";
+		
+		//***************Path Ordinateur Morvan map Midi-pyrénées*********************/
+		//String mapName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/midi-pyrenees.mapgr";
+		//String pathName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/midi-pyrenees.path";
+		
+		//***************Path Ordinateur Morvan map Belgique*********************/
+		String mapName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/belgium.mapgr";
+		String pathName = "C:/Users/Katillär/Documents/INSA/3ir/be_graphes/insa/europe/france/belgium.path";
+		
 		//***************Path Ordinateur Thomas map INSA*********************/
 	    //String pathName = "/home/thomasp/Documents/insa/be_graphes/europe/france/path_fr31insa_rangueil_r2.path";
 	    //String mapName = "/home/thomasp/Documents/insa/be_graphes/europe/france/insa.mapgr";
 		
 		//***************Path Ordinateur Thomas map midi-pyrennees*********************/
-	    String pathName = "/home/thomasp/Documents/insa/be_graphes/europe/france/midi-pyrenees.mapfg";
-	    String mapName = "/home/thomasp/Documents/insa/be_graphes/europe/france/midi-pyrenees.mapgr";
+	    //String pathName = "/home/thomasp/Documents/insa/be_graphes/europe/france/midi-pyrenees.mapfg";
+	    //String mapName = "/home/thomasp/Documents/insa/be_graphes/europe/france/midi-pyrenees.mapgr";
 	    
 
 	    // Create a graph reader.
@@ -46,19 +60,19 @@ public class AStarTest {
 
 
 	    // TODO: Create a PathReader.
-	    PathReader pathReader = new BinaryPathReader(new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
+	    //PathReader pathReader = new BinaryPathReader(new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
 
 	    // TODO: Read the path.
 	    //Path path = pathReader.readPath(graph);
 
-	    int idOrigin=1;
-	    int idDest=121;
+	    int idOrigin=966454;
+	    int idDest=8836;
 	    
 	    List<ArcInspector> L = ArcInspectorFactory.getAllFilters();
 
 		
-		ShortestPathData SP=new ShortestPathData(graph,graph.get(idOrigin),graph.get(idDest),L.get(0));
-		ShortestPathData SPAS=new ShortestPathData(graph,graph.get(idOrigin),graph.get(idDest),L.get(0));
+		ShortestPathData SP=new ShortestPathData(graph,graph.get(idOrigin),graph.get(idDest),L.get(2));
+		ShortestPathData SPAS=new ShortestPathData(graph,graph.get(idOrigin),graph.get(idDest),L.get(2));
 		DijkstraAlgorithm D=new DijkstraAlgorithm(SP);
 		AStarAlgorithm AS = new AStarAlgorithm(SPAS);
 		
@@ -68,7 +82,7 @@ public class AStarTest {
 		long finD=System.currentTimeMillis();
 		long diffD=finD-debD;
 		
-		System.out.println("temps d'Ã©xecution de Dijkstra:"+diffD);
+		System.out.println("temps d'éxecution de Dijkstra:"+diffD);
 		
 		//Calcule du temps d'execution pour AStar
 		long debA=System.currentTimeMillis();
@@ -77,16 +91,32 @@ public class AStarTest {
 		long diffA=finA-debA;
 		
 		//Affichage des temps d'execution par curiosite puisque assertTrue() le vÃ©rifie
-		System.out.println("temps d'Ã©xecution de Astar:"+diffA);
+		System.out.println("temps d'éxecution de Astar:"+diffA);
 		System.out.println("sol: "+SPD);
 		System.out.println("sol: "+SPASS);
 		
+		File ff=new File("C:/Users/Katillär/Documents/INSA/3ir/be_graphes/resultat.txt"); // définir l'arborescence
+		ff.createNewFile();
+		FileWriter ffw=new FileWriter(ff,true);
+		
+		ffw.write("temps d'éxecution de Dijkstra:"+diffD+"\n");  // écrire une ligne dans le fichier resultat.txt
+		ffw.write("temps d'éxecution de Astar:"+diffA+"\n");  // écrire une ligne dans le fichier resultat.txt
+		ffw.write("sol: "+SPD+"\n");  // écrire une ligne dans le fichier resultat.txt
+		ffw.write("sol: "+SPASS+"\n");  // écrire une ligne dans le fichier resultat.txt
+
+		ffw.write("\n"); // forcer le passage à la ligne
+		ffw.close(); // fermer le fichier à la fin des traitements
 		//renvoie exactement la meme valeur donc ok
 		//tests rÃ©alisÃ©s sur plusieurs map
 		
 		assertTrue(diffA<diffD);
 		assertEquals(SPD.getPath().getLength(), SPASS.getPath().getLength(),0.001);
 		
+
+
+
+
+
 	}
 
 }
